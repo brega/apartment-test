903b7aa817abfd4144a1acf4cecd74e807a9ebba
Ivan Arakcheev <wildfiler0@gmail.com>
2014-08-26 18:21:48 +0300

    Change dump format to sql

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module ApartmentMigrationTest
     # config.i18n.default_locale = :de
 
     config.autoload_paths += %W("#{config.root}/lib")
+    config.active_record.schema_format = :sql
   end
 end
